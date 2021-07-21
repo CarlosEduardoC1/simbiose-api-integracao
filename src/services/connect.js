a9d5d525a53c98484327265a44636ccdecc41672
CarlosEduardoC1 <cnobre.trabalho@gmail.com>
2021-07-21 14:53:56 -0300

    updt

--- src/services/connect.js
+++ src/services/connect.js
@@ -1,6 +1,6 @@
 
 const axios = require('axios').default;
 
-const connect = axios.create({ baseURL: `http://localhost:3080/` });
+const connect = axios.create({ baseURL: `http://18.231.163.127:3090/` });
 
 module.exports = connect;
